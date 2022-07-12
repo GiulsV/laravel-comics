7e682bf85243c6920ed88914cf865998ff040a58
GiuliaV <valente.giulia93@gmail.com>
2022-07-12 18:16:40 +0200

    aggiunto bluesection footer + ottimizzato web

---
+++ resources/views/partials/bluesection.blade.php
@@ -0,0 +1,28 @@
+<section>
+    <div class="container-80w">
+       <div class="option-container">
+         <img src="/images/buy-comics-digital-comics.png" alt="digital comics">
+         <a href="#">digital comics</a>
+       </div>
+
+       <div class="option-container">
+         <img src="/images/buy-comics-merchandise.png" alt="dc merchandise">
+         <a href="#">dc merchandise</a>
+       </div>
+
+       <div class="option-container">
+         <img src="/images/buy-comics-subscriptions.png" alt="subscriptions">
+         <a href="#">subscription</a>
+       </div>
+
+       <div class="option-container">
+         <img src="/images/buy-comics-shop-locator.png" alt="shop locator">
+         <a href="#">comic shop locator</a>
+       </div>
+
+       <div class="option-container">
+         <img src="/images/buy-dc-power-visa.svg" alt="buy dc power visa">
+         <a href="#">dc power visa</a>
+       </div> 
+    </div> 
+  </section>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,64 @@
+<footer>
+    <div class="container-90w">
+      <div class="list-container">
+        <ul>
+          <li>dc comics</li>
+          <li><a href="#">Characters</a></li>
+          <li><a href="#">Comics</a></li>
+          <li><a href="#">Movies</a></li>
+          <li><a href="#">TV</a></li>
+          <li><a href="#">Games</a></li>
+          <li><a href="#">Videos</a></li>
+          <li><a href="#">News</a></li>
+        </ul>
+
+        <ul>
+          <li>shop</li>
+          <li><a href="#">Shop DC</a></li>
+          <li><a href="#">Shop DC Collectibles</a></li>
+        </ul>
+      </div>
+
+      <div class="list-container">
+        <ul>
+          <li>dc</li>
+          <li><a href="#">Terms Of Use</a></li>
+          <li><a href="#">Privacy policy (News)</a></li>
+          <li><a href="#">Ad Choices</a></li>
+          <li><a href="#">Advertising</a></li>
+          <li><a href="#">Jobs</a></li>
+          <li><a href="#">Subscriptions</a></li>
+          <li><a href="#">Talent Workshops</a></li>
+          <li><a href="#">CPSC Certificates</a></li>
+          <li><a href="#">Ratings</a></li>
+          <li><a href="#">Shop Help</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+
+      <div class="list-container">
+        <ul>
+          <li>sites</li>
+          <li><a href="#">DC</a></li>
+          <li><a href="#">MAD Magazine</a></li>
+          <li><a href="#">DC Kids</a></li>
+          <li><a href="#">DC Universe</a></li>
+          <li><a href="#">DC Power Visa</a></li>
+        </ul>
+      </div> 
+    </div>
+
+    
+    <div class="social-container">
+      <a class="btn" href="#">sign-up now!</a>
+      <div class="icons">
+        <span>follow us</span>
+        <a href="#"><img src="/images/footer-facebook.png" alt="facebook icon"></a>
+        <a href="#"><img src="/images/footer-twitter.png" alt="twitter icon"></a>
+        <a href="#"><img src="/images/footer-youtube.png" alt="youtube icon"></a>
+        <a href="#"><img src="/images/footer-pinterest.png" alt="pinterest icon"></a>
+        <a href="#"><img src="/images/footer-periscope.png" alt="periscope icon"></a>
+      </div>
+    </div>
+
+</footer>
