16f206a316a0d24761dec9b7d36beb800cec6f28
GiuliaV <valente.giulia93@gmail.com>
2022-07-12 16:28:38 +0200

    aggiunti elementi navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="public/css/style.css">
     <title>Laravel-Comics</title>
 </head>
 <body>
-    <h1>Prova Home</h1>
+    <header>
+        <div class="container-90w">
+            <img src="../public/images/dc-logo.png" alt="DC logo">
+            <div id="menu-container">
+                <ul>
+                @foreach ($navlinks as $navlink) 
+                <li>
+                    <a href="{{ url('/comics') }}">{{ $navlink['text'] }}</a>
+                </li>
+                @endforeach  
+                </ul>
+            </div>
+        </div>
+  </header>
+    <main>
+        <h1>HOME TEST</h1>
+       
+    </main>
+    <footer>
+    </footer>    
 </body>
 </html>
